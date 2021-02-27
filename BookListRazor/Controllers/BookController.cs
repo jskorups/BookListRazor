9a43121ac11c770f626b6b4cfd7065d668e832db
JSkorups <skorupski.jakub@gmail.com>
2021-02-27 18:54:33 +0100

    Delete action and scripts for delete button

--- BookListRazor/Controllers/BookController.cs
+++ BookListRazor/Controllers/BookController.cs
@@ -1,7 +1,9 @@
 ﻿using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace BookListRazor.Controllers
 {
@@ -17,9 +19,22 @@ namespace BookListRazor.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data = _db.Book.ToList() });
+            return Json(new { data =await _db.Book.ToListAsync() });
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete (int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(x => x.Id == id);
+            if(bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete succesfull" });
+
         }
     }
 }
--- BookListRazor/wwwroot/js/booklist.js
+++ BookListRazor/wwwroot/js/booklist.js
@@ -26,7 +26,7 @@ function loadDataTable() {
                     Edit
                     </a>
                     &nbsp;
-                      <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                      <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;' onclick=Delete('/api/book?id='+${data})>
                     Delete
                     </a>
                     </div>`;
@@ -39,4 +39,32 @@ function loadDataTable() {
             "width": "100%"
 
         });
+}
+
+function Delete(url) {
+    swal(
+        {
+            title: "Sure?",
+            text: "Not able to recover",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true
+        }).then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    type: "DELETE",
+                    url: url,
+                    success: function (data) {
+                        if (data.success) {
+                            toastr.success(data.message);
+                            dataTable.ajax.reload();
+                        }
+                        else {
+                            toastr.error(data.message);
+                        }
+
+                    }
+                });
+            }
+        })
 }
