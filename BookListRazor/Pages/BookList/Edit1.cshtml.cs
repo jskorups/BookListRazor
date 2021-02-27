1681ab21a655d4df8e05afca237d33a0d8c21384
JSkorups <skorupski.jakub@gmail.com>
2021-02-27 17:20:21 +0100

    Adding files

---
+++ BookListRazor/Pages/BookList/Edit1.cshtml.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class Edit1Model : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public Edit1Model(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb =await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+
+
+        }
+
+
+      
+    }
+}
